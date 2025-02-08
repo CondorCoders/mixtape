e134e27838149cf148a0cadc733389554ea49156
Condor Coders <condorcoders@gmail.com>
2025-02-08 20:01:15 +1000

    Update conditional names

--- src/components/case-front.tsx
+++ src/components/case-front.tsx
@@ -8,6 +8,8 @@ interface CaseFrontInterface extends MixtapeType {
 export const CaseFront = ({
   from,
   to,
+  name,
+  playlistName,
   message,
   onClick,
 }: CaseFrontInterface) => {
@@ -18,28 +20,35 @@ export const CaseFront = ({
     >
       <div className="aspect-video flex flex-col bg-orange-50 border-4 border-slate-300/25 rounded-xs">
         <div className="w-full bg-orange-200">
-          <div className="flex">
-            <span className="w-1/2 font-bold text-sm md:text-lg font-mono p-4 pb-0">
+          {from || to ? (
+            <div className="w-full flex">
               {from && (
-                <>
-                  From:
-                  <span className="font-shadow text-sm md:text-2xl pl-4 text-blue-800">
-                    {from}
-                  </span>
-                </>
+                <span className="w-1/2 font-bold text-sm md:text-lg font-mono p-4 pb-0">
+                  <>
+                    From:
+                    <span className="font-shadow text-sm md:text-2xl pl-4 text-blue-800">
+                      {from}
+                    </span>
+                  </>
+                </span>
               )}
-            </span>
-            <span className="w-1/2 font-bold text-sm md:text-lg font-mono p-4 pb-0">
               {to && (
-                <>
-                  To:
-                  <span className="font-shadow pl-4 text-sm md:text-2xl text-blue-800">
-                    {to}
-                  </span>
-                </>
+                <span className="w-1/2 font-bold text-sm md:text-lg font-mono p-4 pb-0">
+                  <>
+                    To:
+                    <span className="font-shadow pl-4 text-sm md:text-2xl text-blue-800">
+                      {to}
+                    </span>
+                  </>
+                </span>
               )}
-            </span>
-          </div>
+            </div>
+          ) : (
+            <p className="font-shadow p-4 pb-0 text-sm md:text-2xl text-blue-800">
+              {name || playlistName}
+            </p>
+          )}
+
           <span className="w-full h-[3px] bg-stone-600 block mb-4" />
         </div>
         <div className="flex-1 w-full h-full flex flex-col items-center justify-center p-3 pb-0 ">
